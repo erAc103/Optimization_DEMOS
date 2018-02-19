5b736f6adf50a730ac3cae23d263737efe414f5b
erAc103 <pigeon.airmail.103@gmail.com>
2018-02-19 17:37:20 -0600

    test2

--- Chapter7.py
+++ Chapter7.py
@@ -1,13 +1,23 @@
+import matplotlib.pyplot as plt
 import numpy as np
 
 ''' Various line search methods discussed in chapter 7 '''
 
 
-def fibSearchExample():         # 2-16-2018         Not impressed
+def fibSearchExample(initialRange, accuracy, printIter=False, graph=False):         # 2-16-2018         Not impressed
+    """ Fibonacci Search - Example 7.2, page 98
+    :param initialRange: [min, max]
+    :param accuracy: accuracy of final interval
+    :param printIter: do you want each iteration output printed to the console?
+    :param graph: do you want the function graphed?
+    :return: [a, b] a minimum value from the initial range lies within this range
+    """
 
+    # objective function
     def func(x):
         return x**4 - 14*(x**3) + 60*(x**2) - 70*x
 
+    # retrieves Fibonacci number at a given index
     def fibNum(x):
 
         if x == 1 or x == 0:
@@ -15,15 +25,24 @@ def fibSearchExample():         # 2-16-2018         Not impressed
 
         return fibNum(x-1) + fibNum(x-2)
 
+    if initialRange[0] >= initialRange[1]:
+        print('Fix initial range input')
+        return
 
-    a = 0
-    b = 2
-    e = 0.001         # some small number
+    # used for plotting at the end
+    start1 = initialRange[0]
+    start2 = initialRange[1]
+
+    # changes each iteration
+    a = initialRange[0]
+    b = initialRange[1]
+
+    e = 0.0001         # any small number
     N = 1
 
-    print('iteration # 0', [a, b])
+    iterationOutput = [[a, b]]
 
-    finalRange = 0.0001    # change this to get more accurate answers - similar to demo2Challenge
+    finalRange = accuracy
     initialRange = b - a
 
     while fibNum(N) < (1 + 2*e)/(finalRange/initialRange):
@@ -31,7 +50,6 @@ def fibSearchExample():         # 2-16-2018         Not impressed
 
     N -= 2    # correct for different Fibonacci sequences (with and without 0)
 
-    iteration = 0
     while N != 0:
         rho = 1 - (fibNum(N+1)/fibNum(N+2))
 
@@ -47,7 +65,40 @@ def fibSearchExample():         # 2-16-2018         Not impressed
             b = b1
 
         N -= 1
-        iteration += 1
-        print('Iteration #', iteration, [a, b])
+        iterationOutput.append([a, b])
+
+    def printIterations(iterationOutput):
+        count = 0
+        for x in iterationOutput:
+            print('Iteration #', count, x)
+            count +=1
+
+    def graphFunc():
+        plt.figure()
+        t1 = np.linspace(-0.5, 7.25)
+
+        # plot the function
+        plt.plot(t1, func(t1))
+
+        # plot points at initial range
+        plt.scatter(start1, func(start1), color='lime')
+        plt.scatter(start2, func(start2), color='lime')
+
+        # plot points at final range
+        plt.scatter(a, func(a), color='red')
+        plt.scatter(b, func(b), color='red')
+
+        plt.show()
+
+    if printIter:
+        printIterations(iterationOutput)
+    if graph:
+        graphFunc()
 
     return [a, b]
+
+
+########################################################################################################################
+''' Run code down here '''
+
+
