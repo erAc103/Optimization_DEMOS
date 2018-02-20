715553a852e3c9a365cee4ee8efe1d25e4b9a82b
erAc103 <pigeon.airmail.103@gmail.com>
2018-02-19 19:25:58 -0600

    Gold and Fib Search done

--- Chapter7.py
+++ Chapter7.py
@@ -1,8 +1,83 @@
+import matplotlib
+matplotlib.use('TkAgg')
+
+from matplotlib.animation import FuncAnimation
 import matplotlib.pyplot as plt
 import numpy as np
+import time
 
 ''' Various line search methods discussed in chapter 7 '''
+def goldSearchExample(initialRange, accuracy, printIter=False, graph=False):
+    """ Golden Search - Example 7.1, page 94
+    :param initialRange: [min, max]
+    :param accuracy: accuracy of final interval
+    :param printIter:
+    :param graph:
+    :return: [a, b] a minimum value from the initial range lies within this range
+    """
+    def func(x):
+        return x**4 - 14*(x**3) + 60*(x**2) - 70*x
 
+    if initialRange[0] >= initialRange[1]:
+        print('Fix initial range input')
+        return
+
+    # used for plotting at the end
+    start1 = initialRange[0]
+    start2 = initialRange[1]
+
+    # changes each iteration
+    a = initialRange[0]
+    b = initialRange[1]
+
+    # 1 - golden ratio
+    rho = 0.382
+
+    iterationOutput = [[a, b]]
+
+    while accuracy < abs(b - a):
+        a1 = a + rho * (b - a)
+        b1 = b - rho * (b - a)
+
+        f1 = func(a1)
+        f2 = func(b1)
+
+        if f1 > f2:
+            a = a1
+        else:
+            b = b1
+
+        iterationOutput.append([a, b])
+
+    def printIterations():
+        count = 0
+        for x in iterationOutput:
+            print('Iteration #', count, x)
+            count +=1
+
+    def graphFunc():
+
+        fig = plt.figure()
+        t1 = np.linspace(-0.5, 7)
+
+        plt.plot(t1, func(t1))
+
+        # plot points at initial range
+        plt.scatter(start1, func(start1), color='lime')
+        plt.scatter(start2, func(start2), color='lime')
+
+        # plot points at final range
+        plt.scatter(a, func(a), color='red')
+        plt.scatter(b, func(b), color='red')
+
+        plt.show()
+
+    if printIter:
+        printIterations(iterationOutput)
+    if graph:
+        graphFunc()
+
+    return [a, b]
 
 def fibSearchExample(initialRange, accuracy, printIter=False, graph=False):         # 2-16-2018         Not impressed
     """ Fibonacci Search - Example 7.2, page 98
---
+++ Chapter8.py
@@ -0,0 +1,5 @@
+import numpy as np
+from scipy.optimize import minimize
+
+''' Various gradient methods discussed in class, in chapter 8 '''
+
