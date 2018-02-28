60e00ae3f4dad5b006e4426d8b2895831725ee78
erAc103 <pigeon.airmail.103@gmail.com>
2018-02-28 15:03:28 -0600

    ...

--- Chapter9.py
+++ Chapter9.py
@@ -9,7 +9,7 @@ import numpy as np
 ''' Chapter 9 Newton's Method Examples '''
 
 
-def example1(x0=[3,-1,0,1], iterations=3, printiter=False, graph=True):
+def example1(x0=[3,-1,0,1], iterations=3, printiter=True):
 
     def func(x):
         return (x[0] + 10*x[1])**2 + 5*(x[2] - x[3])**2 + (x[1] - 2*x[2])**4 + 10*(x[0] - x[3])**4
@@ -25,4 +25,34 @@ def example1(x0=[3,-1,0,1], iterations=3, printiter=False, graph=True):
                          [20, 200+12*(x[1] - 2*x[2])**2, -24*(x[1]-2*x[2])**2, 0],
                          [0, -24*(x[1]-2*x[2])**2, 10 + 48*(x[1]-2*x[2])**2, -10],
                          [-120*(x[0]-x[3])**2, 0, -10, 10+120*(x[0]-x[3])**2]])
+
+    count = 0
+
+    xs = [x0]
+    x = x0
+
+    while count < iterations:
+        F = hess(x)
+        invF = np.linalg.inv(F)
+        g = grad(x)
+
+        nextX = np.array([x]).T - np.matmul(invF, g) # numpy transpose not working
+
+        xs.append(nextX.ravel())
+        x = nextX.ravel()
+
+        count +=1
+
+    def printiterations():
+        i = 0
+        for x in xs:
+            print('Iteration #',i,x)
+            i += 1
+
+    if printiter:
+        printiterations()
+
+    print(func(x))
+
+
 
