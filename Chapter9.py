b5510ee352d3d1cc5c9f64a91bf693ff476868d9
erAc103 <pigeon.airmail.103@gmail.com>
2018-02-28 11:01:53 -0600

    Chapter 9 ...

---
+++ Chapter9.py
@@ -0,0 +1,28 @@
+import matplotlib
+matplotlib.use('TkAgg')
+from scipy import optimize as opt
+from matplotlib.animation import FuncAnimation
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+''' Chapter 9 Newton's Method Examples '''
+
+
+def example1(x0=[3,-1,0,1], iterations=3, printiter=False, graph=True):
+
+    def func(x):
+        return (x[0] + 10*x[1])**2 + 5*(x[2] - x[3])**2 + (x[1] - 2*x[2])**4 + 10*(x[0] - x[3])**4
+
+    def grad(x):
+        return np.array([[2*(x[0] + 10*x[1]) + 40*(x[0] - x[3])**3],
+                         [20*(x[0] + 10*x[1]) + 4*(x[1] - 2*x[2])**3],
+                         [10*(x[2] - x[3]) - 8*(x[1] - 2*x[2])**3],
+                         [-10*(x[2] - x[3]) - 40*(x[0] - x[3])**3]])
+
+    def hess(x):
+        return np.array([[2 + 120*(x[0]-x[3])**2, 20, 0, -120*(x[0]-x[3])**2],
+                         [20, 200+12*(x[1] - 2*x[2])**2, -24*(x[1]-2*x[2])**2, 0],
+                         [0, -24*(x[1]-2*x[2])**2, 10 + 48*(x[1]-2*x[2])**2, -10],
+                         [-120*(x[0]-x[3])**2, 0, -10, 10+120*(x[0]-x[3])**2]])
+
--- Driver.py
+++ Driver.py
@@ -6,11 +6,9 @@ import Plots
 ''' Toss code in here to test instead of at the end of the scripts'''
 
 if __name__ == '__main__':
-
-    A = np.array([[3,2],[2,3]])
-    b = np.array([[5],[5]])
-
-    print(ch8.matLeftDivide(A, b))
+    ch8.gradDescent2([4,5], printiter=True)
+    print()
+    ch8.gradDescent4([4,5], printiter=True)
 
 
 
