f30d2fcd3cad207e1670607f87df61c89290bccb
erAc103 <pigeon.airmail.103@gmail.com>
2018-03-08 12:48:27 -0600

    added non-linear regression

--- Chapter9.py
+++ Chapter9.py
@@ -174,7 +174,27 @@ def linRegression1(degree= '1'):
 
 def nonLinearRegression():
 
-    x = [0.9, 1.5, 13.8, 19.8, 24.1, 28.2, 35.2, 60.3, 74.6, 81.3]
-    y - [455.2, 428.6, 124.1, 67.3, 43.2, 28.1, 13.1, -.4, -1.3, -1.5]
+    x = np.array([0.9, 1.5, 13.8, 19.8, 24.1, 28.2, 35.2, 60.3, 74.6, 81.3])
+    y = np.array([455.2, 428.6, 124.1, 67.3, 43.2, 28.1, 13.1, -.4, -1.3, -1.5])
+
+    beta = np.random.randn(1, 2)
+
+    def fit(x, b):
+        return b[0]*np.exp(x * b[1])
+
+    def cost(b):
+        J = 0.5 * np.sum((y - fit(x, b)) ** 2)
+        return J
+
+    sol = opt.minimize(cost, beta)
+
+    print(sol)
+    beta = np.array([sol.x[0], sol.x[1]])
+
+    xspace = np.linspace(0, 100)
+    plt.plot(xspace, fit(xspace, beta))
+    plt.plot(x, y, 'o')
+    plt.show()
+
 
 
