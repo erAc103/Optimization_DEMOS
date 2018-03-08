f393113409479c9931703ba9ebb01f500ae088d3
erAc103 <pigeon.airmail.103@gmail.com>
2018-03-07 19:32:21 -0600

    lin regression added.

--- Chapter9.py
+++ Chapter9.py
@@ -54,60 +54,41 @@ def example1(x0=[3,-1,0,1], iterations=3, printiter=True):
 
     print(func(x))
 
-'''
-def linRegression(points=[[1,2], [2,1]):
-
-    beta = np.random.randn(1,2)
-
-    x = np.array([point[0] for point in points])
-    y = np.array([point[1] for point in points])
-
-    def func(x, b):
-        b = b.ravel()
-        yHat = b[0] + x*b[1]
-        return yHat
-
-    def cost(b):
-        J = 0.5 * np.sum((y-func(x, b))**2)
-        return J
-
-    def fit(x):
-        return beta[0] + x*beta[1]
-
-    sol = opt.minimize(cost, beta)
-
-    beta = np.array([sol.x[0], sol.x[1]])
-    print(beta)
-
-    fig = plt.figure()
-    plt.ylim(0,7)
-    plt.xlim(0,7)
-
-    xspace = np.linspace(0,7)
-    print(xspace)
-    plt.plot(xspace, fit(xspace))
-    plt.plot(x,y, 'o')
-    plt.show()
-
-'''
 def linRegression1(degree= '1'):
 
     def deg1():
 
         beta = np.random.randn(1, 2)
 
-        def func(x):
-            return 1 + 0.75*x
+        def func(x):  # y
+            return .5 + 0.5 * x
 
-        def funcData(x):
-            return 1 + 0.75*x + np.random.normal(0, 0.3)
+        def funcData(x):  # produces y data with some error
+            return np.random.normal(x, .25)
 
-        def fit(x, b):
-            return b[0] + b[1]*x
+        def fit(x, b):  # yHat
+            return b[0] + b[1] * x
+
+        x = np.linspace(0, 4, 30)
+        y = funcData(func(x))
+        ystuff = func(x)
 
         def cost(b):
-            J = 0.5 * np.sum((y - fit(x)) ** 2)
+            J = 0.5 * np.sum((y - fit(x, b)) ** 2)
             return J
+
+        sol = opt.minimize(cost, beta)
+
+        print(sol)
+        beta = np.array([sol.x[0], sol.x[1]])
+
+        fig = plt.figure()
+        plt.xlim(0, 4)
+
+        xspace = np.linspace(0, 4)
+        plt.plot(xspace, fit(xspace, beta))
+        plt.plot(x, y, 'o')
+        plt.show()
 
 
 
@@ -115,39 +96,54 @@ def linRegression1(degree= '1'):
 
         beta = np.random.randn(1, 3)
 
-        def func(x):
-            return 5 + 0.4*x - x**2
+        def func(x):  # y
+            return .5 + 0.5 * x - 2 * x ** 2
 
-        def funcData(x):
-            return 5 + 0.4*x - x**2 + np.random.normal(0, 0.3)
+        def funcData(x):  # produces y data with some error
+            return np.random.normal(x, 4)
 
-        def fit(x):
-            return beta[0] + beta[1]*x + beta[2]*x**2
+        def fit(x, b):  # yHat
+            return b[0] + b[1] * x + b[2] * x ** 2
+
+        x = np.linspace(0, 4, 30)
+        y = funcData(func(x))
+        ystuff = func(x)
 
         def cost(b):
-            J = 0.5 * np.sum((y - func(x)) ** 2)
+            J = 0.5 * np.sum((y - fit(x, b)) ** 2)
             return J
+
+        sol = opt.minimize(cost, beta)
+
+        print(sol)
+        beta = np.array([sol.x[0], sol.x[1], sol.x[2]])
+
+        fig = plt.figure()
+        plt.xlim(0,4)
+
+        xspace = np.linspace(0, 4)
+        plt.plot(xspace, fit(xspace, beta))
+        plt.plot(x, y, 'o')
+        plt.show()
 
 
     def deg3():
 
         beta = np.random.randn(1, 4)
+        print(beta)
 
         def func(x): # y
             return .5 + 0.5*x - 2*x**2 + 1.1*x**3
 
         def funcData(x): # produces y data with some error
-            return .5 + 0.5*x - 2*x**2 + 1.1*x**3 + np.random.normal(0, 3)
+            return np.random.normal(x, 4)
 
         def fit(x, b): # yHat
             return b[0] + b[1]*x + b[2]*x**2 + b[3]*x**3
 
-        x = np.linspace(0, 4, 20)
-        y = funcData(x)
+        x = np.linspace(0, 4, 30)
+        y = funcData(func(x))
         ystuff = func(x)
-
-        print(y)
-        print(ystuff)
 
         def cost(b):
             J = 0.5 * np.sum((y - fit(x, b)) ** 2)
@@ -174,3 +170,11 @@ def linRegression1(degree= '1'):
         deg3()
     else:
         print('Wrong degree number')
+
+
+def nonLinearRegression():
+
+    x = [0.9, 1.5, 13.8, 19.8, 24.1, 28.2, 35.2, 60.3, 74.6, 81.3]
+    y - [455.2, 428.6, 124.1, 67.3, 43.2, 28.1, 13.1, -.4, -1.3, -1.5]
+
+
